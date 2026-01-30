42c7a9d344c034171c9ab848693a925cd40c70c7
i am Chuppyy <62337417+chuppyy@users.noreply.github.com>
2026-01-30 13:46:26 +0700

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,8 +8,10 @@ export default function MyApp({ Component, pageProps }: AppProps) {
     <>
       <Head>
         <meta name="viewport" content="initial-scale=1, width=device-width" />
-        <script defer src="https://videoadstech.org/ads/anhus_livextop_com.974c98a8-3d82-43b5-abe5-429a5345fb2b.video.js"></script>
-        <script async src="https://server.adhub.media/ads/anhus_livextop_com.4a3f4e8c-a8d5-4056-a2d3-68d8a9afaf39.display.js"></script>
+        {/*<script defer src="https://videoadstech.org/ads/anhus_livextop_com.974c98a8-3d82-43b5-abe5-429a5345fb2b.video.js"></script>
+        <script async src="https://server.adhub.media/ads/anhus_livextop_com.4a3f4e8c-a8d5-4056-a2d3-68d8a9afaf39.display.js"></script>*/}
+        <script defer src="https://videoadstech.org/ads/anhus_daily24_blog.88dfa97d-31cc-4294-b247-9af265543343.video.js"></script>
+        <script async src="https://server.adhub.media/ads/anhus_daily24_blog.80d33374-7854-4850-92e1-f725ed778c04.display.js"></script>
       </Head>
 
       <Layout>
